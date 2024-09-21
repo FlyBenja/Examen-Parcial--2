62096614358da5a85b5bb9628673d8c53a5f2dc7
FlyBenja <aldanabenjamin185@gmail.com>
2024-09-21 17:41:40 -0600

    Subir pagina con diseño

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function fetchData() {
 function displayResults(data) {
     const resultsDiv = document.getElementById('resultados');
     resultsDiv.innerHTML = `
-        <div class="card mb-3">
+        <div class="card mb-3 w-100">
             <div class="card-header bg-dark text-white">
                 Datos del Departamento: ${data.nombre}
             </div>
@@ -27,41 +27,45 @@ function displayResults(data) {
             </div>
         </div>
 
-        ${createDataSection('Índices y Promedios', [
-            { label: 'Índice de Masculinidad', value: data.indice_masculinidad },
-            { label: 'Promedio de Hijos por Mujer', value: data.prom_hijos_mujer },
-            { label: 'Edad Promedio', value: data.edad_promedio }
-        ])}
-        ${createDataSection('Población por Sexo', [
-            { label: 'Hombres', value: formatPercentage(data.total_sexo_hombre, data.pob_total) },
-            { label: 'Mujeres', value: formatPercentage(data.total_sexo_mujeres, data.pob_total) }
-        ])}
-        ${createDataSection('Población por Grupos de Edad', [
-            { label: '0-14 años', value: formatPercentage(data.pob_edad_014, data.pob_total) },
-            { label: '15-64 años', value: formatPercentage(data.pob_edad_1564, data.pob_total) },
-            { label: '65+ años', value: formatPercentage(data.pob_edad_65, data.pob_total) }
-        ])}
-        ${createDataSection('Población por Área', [
-            { label: 'Urbana', value: formatPercentage(data.total_sector_urbano, data.pob_total) },
-            { label: 'Rural', value: formatPercentage(data.total_sector_rural, data.pob_total) }
-        ])}
-        ${createDataSection('Población por Pueblos', [
-            { label: 'Maya', value: formatPercentage(data.pob_pueblo_maya, data.pob_total) },
-            { label: 'Garífuna', value: formatPercentage(data.pob_pueblo_garifuna, data.pob_total) },
-            { label: 'Xinca', value: formatPercentage(data.pob_pueblo_xinca, data.pob_total) },
-            { label: 'Afrodescendiente/Otros', value: formatPercentage(data.pob_pueblo_afrodescendiente, data.pob_total) },
-            { label: 'Ladino', value: formatPercentage(data.pob_pueblo_ladino, data.pob_total) },
-            { label: 'Extranjero', value: formatPercentage(data.pob_pueblo_extranjero, data.pob_total) }
-        ])}
+        <div class="row">
+            ${createDataSection('Índices y Promedios', [
+                { label: 'Índice de Masculinidad', value: data.indice_masculinidad },
+                { label: 'Promedio de Hijos por Mujer', value: data.prom_hijos_mujer },
+                { label: 'Edad Promedio', value: data.edad_promedio }
+            ])}
+            ${createDataSection('Población por Sexo', [
+                { label: 'Hombres', value: formatPercentage(data.total_sexo_hombre, data.pob_total) },
+                { label: 'Mujeres', value: formatPercentage(data.total_sexo_mujeres, data.pob_total) }
+            ])}
+            ${createDataSection('Población por Grupos de Edad', [
+                { label: '0-14 años', value: formatPercentage(data.pob_edad_014, data.pob_total) },
+                { label: '15-64 años', value: formatPercentage(data.pob_edad_1564, data.pob_total) },
+                { label: '65+ años', value: formatPercentage(data.pob_edad_65, data.pob_total) }
+            ])}
+            ${createDataSection('Población por Área', [
+                { label: 'Urbana', value: formatPercentage(data.total_sector_urbano, data.pob_total) },
+                { label: 'Rural', value: formatPercentage(data.total_sector_rural, data.pob_total) }
+            ])}
+            ${createDataSection('Población por Pueblos', [
+                { label: 'Maya', value: formatPercentage(data.pob_pueblo_maya, data.pob_total) },
+                { label: 'Garífuna', value: formatPercentage(data.pob_pueblo_garifuna, data.pob_total) },
+                { label: 'Xinca', value: formatPercentage(data.pob_pueblo_xinca, data.pob_total) },
+                { label: 'Afrodescendiente/Otros', value: formatPercentage(data.pob_pueblo_afrodescendiente, data.pob_total) },
+                { label: 'Ladino', value: formatPercentage(data.pob_pueblo_ladino, data.pob_total) },
+                { label: 'Extranjero', value: formatPercentage(data.pob_pueblo_extranjero, data.pob_total) }
+            ])}
+        </div>
     `;
 }
 
 function createDataSection(title, dataItems) {
     return `
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                ${dataItems.map(item => `<p class="card-text"><strong>${item.label}:</strong> ${item.value}</p>`).join('')}
+        <div class="col-md-4 mb-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    ${dataItems.map(item => `<p class="card-text"><strong>${item.label}:</strong> ${item.value}</p>`).join('')}
+                </div>
             </div>
         </div>
     `;
@@ -70,4 +74,3 @@ function createDataSection(title, dataItems) {
 function formatPercentage(value, total) {
     return `${(value / total * 100).toFixed(2)}%`;
 }
-
